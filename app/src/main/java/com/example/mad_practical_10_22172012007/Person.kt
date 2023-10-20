5df13e04b624b3dc47a1c086505b2e01325195b0
student <student@gnu.ac.in>
2023-10-20 12:33:35 +0530

    hey

---
+++ app/src/main/java/com/example/mad_practical_10_22172012007/Person.kt
@@ -0,0 +1,4 @@
+package com.example.mad_practical_10_22172012007
+
+class Person(var id:String,var name:String,var emailId:String,var phoneNo:String,var address:String,var latitude:Double,var longitude:Double) {
+}
